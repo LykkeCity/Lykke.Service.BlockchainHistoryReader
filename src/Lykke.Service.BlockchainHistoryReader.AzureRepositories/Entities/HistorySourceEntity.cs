b6f4c6c59389f24c77ed8a59f066157e2978bda9
Yury Glushkov <mail@glushkov.us>
2018-09-13 10:53:29 +0300

    Missed attribute added

--- src/Lykke.Service.BlockchainHistoryReader.AzureRepositories/Entities/HistorySourceEntity.cs
+++ src/Lykke.Service.BlockchainHistoryReader.AzureRepositories/Entities/HistorySourceEntity.cs
@@ -1,8 +1,11 @@
 using System;
 using Lykke.AzureStorage.Tables;
+using Lykke.AzureStorage.Tables.Entity.Annotation;
+using Lykke.AzureStorage.Tables.Entity.ValueTypesMerging;
 
 namespace Lykke.Service.BlockchainHistoryReader.AzureRepositories.Entities
 {
+    [ValueTypeMergingStrategy(ValueTypeMergingStrategy.UpdateIfDirty)]
     public class HistorySourceEntity : AzureTableEntity
     {
         private DateTime _historyUpdatedOn;
