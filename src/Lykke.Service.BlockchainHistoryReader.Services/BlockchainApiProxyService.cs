138cf5f2b9c1aba8f2d4a22c9e6fb969a3a1babe
Yury Glushkov <mail@glushkov.us>
2018-10-15 10:27:25 +0300

    Asset accuracy caching added

--- src/Lykke.Service.BlockchainHistoryReader.Services/BlockchainApiProxyService.cs
+++ src/Lykke.Service.BlockchainHistoryReader.Services/BlockchainApiProxyService.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
+using Lykke.Common.Cache;
 using Lykke.Common.Log;
 using Lykke.Service.BlockchainApi.Client;
 using Lykke.Service.BlockchainApi.Client.Models;
@@ -15,6 +17,7 @@ namespace Lykke.Service.BlockchainHistoryReader.Services
     [UsedImplicitly]
     public class BlockchainApiProxyService : IBlockchainApiProxyService
     {
+        private readonly OnDemandDataCache<AssetAccuracy> _assetAccuracyCache;
         private readonly ConcurrentDictionary<string, IBlockchainApiClient> _blockchainApiClients;
         private readonly IBlockchainSettingsClient _blockchainSettingsClient;
         private readonly ILogFactory _logFactory;
@@ -24,6 +27,7 @@ namespace Lykke.Service.BlockchainHistoryReader.Services
             IBlockchainSettingsClient blockchainSettingsClient,
             ILogFactory logFactory)
         {
+            _assetAccuracyCache = new OnDemandDataCache<AssetAccuracy>();
             _blockchainApiClients = new ConcurrentDictionary<string, IBlockchainApiClient>();
             _blockchainSettingsClient = blockchainSettingsClient;
             _logFactory = logFactory;
@@ -40,26 +44,52 @@ namespace Lykke.Service.BlockchainHistoryReader.Services
 
             if (blockchainSettings != null)
             {
-                var blockchainClient = _blockchainApiClients.GetOrAdd
+                var blockchainApiClient = _blockchainApiClients.GetOrAdd
                 (
                     blockchainSettings.ApiUrl, 
                     (apiUrl, logFactory) => new BlockchainApiClient(logFactory, apiUrl),
                     _logFactory
                 );
 
-                return await blockchainClient.GetHistoryOfIncomingTransactionsAsync
+                return await blockchainApiClient.GetHistoryOfIncomingTransactionsAsync
                 (
                     address: address,
                     afterHash: afterHash,
                     take: take,
-                    assetAccuracyProvider: assetId => blockchainClient.GetAssetAsync(assetId).Result.Accuracy
+                    assetAccuracyProvider: assetId => GetAssetAccuracy(blockchainType, blockchainApiClient, assetId)
                 );
             }
             else
             {
                 return Enumerable.Empty<HistoricalTransaction>();
-                
             }
+        }
+
+        private int GetAssetAccuracy(
+            string blockchainType,
+            IBlockchainApiClient blockchainApiClient,
+            string assetId)
+        {
+            var assetAccuracy = _assetAccuracyCache.GetOrAdd
+            (
+                $"{blockchainType}-{assetId}",
+                key => new AssetAccuracy(blockchainApiClient.GetAssetAsync(assetId).Result.Accuracy),
+                TimeSpan.FromMinutes(15)
+            );
+
+            return assetAccuracy.Value;
+        }
+        
+        // OnDemandDataCache requires reference type for value
+        private class AssetAccuracy
+        {
+            public AssetAccuracy(
+                int value)
+            {
+                Value = value;
+            }
+            
+            public int Value { get; }
         }
     }
 }
